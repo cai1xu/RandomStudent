c85fff380a89350c00a0664ae4d8f76665d6099b
cai1xu <47081443+cai1xu@users.noreply.github.com>
2020-09-12 23:49:12 +0800

    NAME with 123

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace RandomStudent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(138, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(630, 103);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始 生成名单(不重复)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.numericUpDown1.Location = new System.Drawing.Point(13, 335);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 369);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(36, 19);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.Text = "3";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(13, 394);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(36, 19);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "5";
+            this.radioButton2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(13, 419);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(58, 19);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "其他";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.Font = new System.Drawing.Font("宋体", 15F);
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(775, 317);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Z12随机姓名生成 开源地址:github.com/cai1xu/RandomStudent";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace RandomStudent
+{
+    //名单来自 live.k12top.com 通过筛选获得
+    public partial class Form1 : Form
+    {
+        string[] z12List = null;
+        public Form1()
+        {
+            //名单来自 https://live.k12top.com:8025/Moral/Query/QueryStudentList (参数已省略)通过json解析得到
+            
+            /*
+            
+            int cnt = 0;
+            var json = JObject.Parse(File.ReadAllText(@"c:\temp\Studentlist"));
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in json["Data"])
+            {
+                if (item["EduYearName"].ToString() == "高2020级" && item["TeacherName"].ToString() == "**乐") 姓名已省略
+                {
+                    cnt++;
+                    sb.Append(item["Name"].ToString() + ",");
+                }
+            }
+            _ = sb.ToString();
+            
+            */
+            string source = "学生名称英文逗号间隔";
+            z12List = source.Split(',');        
+            InitializeComponent();
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            this.Focus();
+            this.DoubleBuffered = true;
+#if DEBUG
+            //ApplyResult(z12List);
+            //textBox1.Text = Resource1.Z12List;
+            //this.ApplyResult(z12List);
+#endif
+        }
+        private void ApplyResult(string[] res)
+        {
+            if (res.Length == 0)
+                return;
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in res)
+            {
+                //windows的\r\n真的烦，直接\n不就好了么，浪费1个char的内存
+                sb.Append($"{item}\r\n");
+            }
+            textBox1.Text = sb.ToString().Substring(0, sb.ToString().Length - 1);
+
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = numericUpDown1.Value < 1 ? 1 : numericUpDown1.Value > z12List.Length ? z12List.Length : numericUpDown1.Value;
+            if (numericUpDown1.Value == 3)
+            {
+                this.radioButton1.Checked = true;
+            }
+            else if (numericUpDown1.Value == 5)
+            {
+                this.radioButton2.Checked = true;
+            }
+            else
+            {
+                this.radioButton3.Checked = true;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == true)
+                this.numericUpDown1.Value = 5;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+                this.numericUpDown1.Value = 3;
+        }
+
+        bool end = true;
+        /// <summary>
+        /// //随机数发生器 说不公平的杠精自己看
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //开始
+            if (end)
+            {
+                end = false;
+                button1.Text = "停止 生成名单(不重复)";
+                begin();
+            }
+            else
+            {
+                button1.Text = "开始 生成名单(不重复)";
+                end = true;
+            }
+        }
+
+        private void begin()
+        {
+            while (!end)
+            {
+                PickList();
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+        private void PickList()
+        {
+
+            //textBox1.Clear();
+            int cnt = Convert.ToInt32(numericUpDown1.Value);
+            //拷贝静态的字符串数组至动态数字，方便删除操作
+            List<string> lst = new List<string>();
+            foreach (var item in z12List)
+            {
+                lst.Add(item);
+            }
+            string[] generated = new string[cnt];
+            for (int i = 0; i < cnt; i++)
+            {
+                int n = PickNumber(0, lst.Count - 1);
+                string name = lst[n];
+                generated[i] = name;
+                lst.Remove(name);
+            }
+            ApplyResult(generated);
+        }
+        /// <summary>
+        /// //随机数发生器 说不公平的杠精自己看
+        /// </summary>
+        private int PickNumber(int min,int max)
+        {
+            byte[] randomBytes = new byte[4 /*Int32长度*/ ];
+            RNGCryptoServiceProvider rngCrypto =new RNGCryptoServiceProvider();
+            
+            // From MSDN:
+            //
+            // 摘要:
+            //     用经过加密的强随机值序列填充字节数组。
+            //
+            // 参数:
+            //   data:
+            //     用经过加密的强随机值序列填充的数组。
+            //
+            // 异常:
+            //   T:System.Security.Cryptography.CryptographicException:
+            //     无法获取加密服务提供程序 (CSP)。
+            //
+            //   T:System.ArgumentNullException:
+            //     data 为 null。
+            rngCrypto.GetBytes(randomBytes);
+            int res = BitConverter.ToInt32(randomBytes, 0);
+            return (res > -1 ? res : - res) % (max - min + 1) + min;
+        }
+    }
+}
